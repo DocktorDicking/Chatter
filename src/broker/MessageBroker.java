47fab82d43fa06aff6ee4b9f59d828955b6407af
Jim <jvw91@msn.com>
2024-03-14 17:03:45 +0100

    Adding more structure to this project so we can work on this later.

---
+++ src/broker/MessageBroker.java
@@ -0,0 +1,8 @@
+package broker;
+
+/**
+ * Will have responsibility for handling the chatmessaging between chats.
+ */
+public class MessageBroker {
+    // TODO
+}
---
+++ src/cli/commandRunner.java
@@ -0,0 +1,8 @@
+package cli;
+
+/**
+ * Will be responsible for the CLI usage of this application.
+ * Since we are not sure if we ever gonna create a gui for this let's go with this for now.
+ */
+public class commandRunner {
+}
